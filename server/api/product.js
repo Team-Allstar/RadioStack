628e38c17429d98f8e777d995a88498176551994
Josh Feinstein <pillboxmediainc@gmail.com>
2019-01-08 17:05:49 -0500

    Update product.js

--- server/api/product.js
+++ server/api/product.js
@@ -2,12 +2,23 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
+router.get('/', async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll()
+    res.json(allProducts)
+  } catch (error) {
+    res.status(500)
+    next(error)
+  }
+})
+
 router.get('/:productId', async (req, res, next) => {
   try {
     const productId = req.params.productId
     const singleProduct = await Product.findById(productId)
-    res.json(singleProduct)
+    res.status(200)res.json(singleProduct)
   } catch (error) {
+    res.status(404)
     next(error)
   }
 })
