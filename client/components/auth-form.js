3413035858e01595fe722e1883815b2adfb9793f
Preet Gudimella <preet.gudimella@gmail.com>
2019-01-10 13:03:26 -0500

    logging-in

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -18,12 +18,14 @@ const AuthForm = props => {
           </label>
           <input name="email" type="text" />
         </div>
+
         <div>
           <label htmlFor="password">
             <small>Password</small>
           </label>
           <input name="password" type="password" />
         </div>
+
         <div>
           <button type="submit">{displayName}</button>
         </div>
