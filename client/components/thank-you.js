b00c29458f5ccadd277c229cc5b92486c8eda490
Josh Feinstein <pillboxmediainc@gmail.com>
2019-01-15 18:19:25 -0500

    bug fixes

--- client/components/thank-you.js
+++ client/components/thank-you.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ThankYou = () => {
   return (
     <div id="thank-you">
-      <p>Thank you for your purchase!</p>
+      <h1>Thank you for your purchase!</h1>
     </div>
   )
 }
